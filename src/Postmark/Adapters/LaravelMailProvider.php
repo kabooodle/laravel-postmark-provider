c0ad9aee8a0b764e707c8e2d8e475b52d442c15c
Jake Toolson <jaketoolson@gmail.com>
2017-04-17 23:24:12 -0700

    Laravel 5.3 changes

--- src/Postmark/Adapters/LaravelMailProvider.php
+++ src/Postmark/Adapters/LaravelMailProvider.php
@@ -1,78 +1,92 @@
-<?php namespace Postmark\Adapters;
+<?php
+namespace Postmark\Adapters;
 
 use Illuminate\Mail\Mailer;
 use Illuminate\Support\ServiceProvider;
 use Swift_Mailer;
 
-class LaravelMailProvider extends ServiceProvider {
+class LaravelMailProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
 
-	protected $defer = true;
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+            $this->registerSwiftMailer();
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register() {
-		$this->app->singleton('mailer', function ($app) {
-			$this->registerSwiftMailer();
+            $this->app->singleton('mailer', function ($app) {
+            // Once we have create the mailer instance, we will set a container instance
+            // on the mailer. This allows us to resolve mailer classes via containers
+            // for maximum testability on said classes instead of passing Closures.
+            $mailer = new Mailer(
+                $app['view'], $app['swift.mailer'], $app['events']
+            );
 
-			// Once we have create the mailer instance, we will set a container instance
-			// on the mailer. This allows us to resolve mailer classes via containers
-			// for maximum testability on said classes instead of passing Closures.
-			$mailer = new Mailer(
-				$app['view'], $app['swift.mailer'], $app['events']
-			);
+            $this->setMailerDependencies($mailer, $app);
 
-			$this->setMailerDependencies($mailer, $app);
+            // If a "from" address is set, we will set it on the mailer so that all mail
+            // messages sent by the applications will utilize the same "from" address
+            // on each one, which makes the developer's life a lot more convenient.
+            $from = $app['config']['mail.from'];
 
-			// If a "from" address is set, we will set it on the mailer so that all mail
-			// messages sent by the applications will utilize the same "from" address
-			// on each one, which makes the developer's life a lot more convenient.
-			$from = $app['config']['mail.from'];
+            if (is_array($from) && isset($from['address'])) {
+                $mailer->alwaysFrom($from['address'], $from['name']);
+            }
 
-			if (is_array($from) && isset($from['address'])) {
-				$mailer->alwaysFrom($from['address'], $from['name']);
-			}
+            $to = $app['config']['mail.to'];
 
-			return $mailer;
-		});
-	}
+            if (is_array($to) && isset($to['address'])) {
+                $mailer->alwaysTo($to['address'], $to['name']);
+            }
 
-	/**
-	 * Set a few dependencies on the mailer instance.
-	 *
-	 * @param  \Illuminate\Mail\Mailer  $mailer
-	 * @param  \Illuminate\Foundation\Application  $app
-	 * @return void
-	 */
-	protected function setMailerDependencies($mailer, $app) {
-		$mailer->setContainer($app);
+            return $mailer;
+        });
+    }
 
-		if ($app->bound('queue')) {
-			$mailer->setQueue($app['queue.connection']);
-		}
-	}
+    /**
+     * Set a few dependencies on the mailer instance.
+     *
+     * @param  \Illuminate\Mail\Mailer  $mailer
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function setMailerDependencies($mailer, $app)
+    {
+        $mailer->setContainer($app);
 
-	/**
-	 * Register the Swift Mailer instance.
-	 *
-	 * @return void
-	 */
-	public function registerSwiftMailer() {
-		$this->app['swift.mailer'] = $this->app->share(function ($app) {
-			$token = $this->app['config']->get('services.postmark');
-			return new Swift_Mailer(new \Postmark\Transport($token));
-		});
-	}
+        if ($app->bound('queue')) {
+            $mailer->setQueue($app['queue']);
+        }
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides() {
-		return ['mailer', 'swift.mailer'];
-	}
+    /**
+     * Register the Swift Mailer instance.
+     *
+     * @return void
+     */
+    public function registerSwiftMailer() {
+        $this->app['swift.mailer'] = $this->app->share(function ($app) {
+            $token = $this->app['config']->get('services.postmark');
+            return new Swift_Mailer(new \Postmark\Transport($token));
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+        return ['mailer', 'swift.mailer'];
+    }
 
 }
